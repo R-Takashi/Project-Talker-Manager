cc2d67beba8d81bcb6d5ea5ab4178916e190f580
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-08-03 15:22:47 -0300

    feat: endpoint POST /talker (requisito 5)

--- index.js
+++ index.js
@@ -51,6 +51,89 @@ const validatePassword = (req, res, next) => {
   next();
 };
 
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+  next();
+};
+
+const validateTalkerName = (req, res, next) => {
+  const newTalker = req.body;
+  if (!newTalker.name) {
+    return res.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (newTalker.name.length < 3) {
+    return res.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+  next();
+};
+
+const validateTalkerAge = (req, res, next) => {
+  const newTalker = req.body;
+
+  if (!newTalker.age || !Number.isInteger(newTalker.age)) {
+    return res.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (newTalker.age < 18) {
+    return res.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+  next();
+};
+
+const validateTalkerDate = (req, res, next) => {
+  const newTalker = req.body;
+  const { talk } = newTalker;
+  const regexDate = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  if (!talk) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório',
+    });
+  } if (!talk.watchedAt) {
+    return res.status(400).json({
+      message: 'O campo "watchedAt" é obrigatório',
+    });
+  } if (!regexDate.test(talk.watchedAt)) {
+    return res.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  next();
+};
+
+const validateTalkerRate = (req, res, next) => {
+  const newTalker = req.body;
+  const { talk } = newTalker;
+
+  if (!talk.rate) {
+    return res.status(400).json({
+      message: 'O campo "rate" é obrigatório',
+    });
+  }
+  if (!Number.isInteger(talk.rate) || talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -76,6 +159,21 @@ app.get('/talker/:id', async (req, res) => {
 app.post('/login', validateEmail, validatePassword, (req, res) => {
   const token = getToken();
   return res.status(200).json({ token });
+});
+
+app.post('/talker', validateToken, validateTalkerName,
+validateTalkerAge, validateTalkerDate, validateTalkerRate, async (req, res) => {
+  const listTalkers = await getTalkers();
+
+  const newTalker = req.body;
+  const addTalker = {
+    id: listTalkers.length + 1,
+    ...newTalker,
+  };
+
+  listTalkers.push(addTalker);
+  await fs.writeFile('./talker.json', JSON.stringify(listTalkers));
+  return res.status(201).json(addTalker);
 });
 
 app.listen(PORT, () => {
