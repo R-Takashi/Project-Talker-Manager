72c59ec3c46f14321b395f0c290ecf80038ce972
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-08-03 12:19:26 -0300

    feat: endpoint GET /talker/:id (requisito 2)

--- index.js
+++ index.js
@@ -8,16 +8,31 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const getTalkers = async () => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+  return JSON.parse(talkers);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', async (req, res) => {
-  const result = await fs.readFile('./talker.json', 'utf-8');
-  const listTalkers = JSON.parse(result);
+  const listTalkers = await getTalkers();
 
   return res.status(200).json(listTalkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const listTalkers = await getTalkers();
+
+    const talker = listTalkers.find(({ id }) => id === +req.params.id);
+
+    if (!talker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
